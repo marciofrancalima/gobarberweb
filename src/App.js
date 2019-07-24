a0e34dda920c3e5809a3175075eaa17d890587ad
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-24 16:41:23 -0300

    Configured Redux (and its initial structure) and changed reactotronConfig to connect with redux and redux saga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
@@ -7,12 +8,16 @@ import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
